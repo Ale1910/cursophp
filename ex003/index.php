e5883eb5d4d5799f1c003aeec6533ec1d604f95f
Ale1910 <128859995+Ale1910@users.noreply.github.com>
2024-04-01 12:41:07 -0300

    Create index.php

---
+++ ex003/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=h1, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+    <?php 
+        // 0x = hexadecimal 0b = binário 0 = Octal
+        // $num = 010;
+        // echo "O valor da variável é $num";
+
+        // $v = 45;
+        // var_dump($v);
+
+        // $num = 3e2; 
+        // echo "O Valor é $num";
+    ?>
+</body>
+</html>
