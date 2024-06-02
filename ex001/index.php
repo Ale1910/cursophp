bf3e0641607532bfab2e5d99d04ea04b51deea29
Ale1910 <128859995+Ale1910@users.noreply.github.com>
2024-06-02 14:15:58 -0300

    Update index.php

--- ex001/index.php
+++ ex001/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dados do servidor</title>
+    <title>sss do servidor</title>
 </head>
 <body>
     <h1>Dados do servidor</h1>
